4c3085dfd20e68fc569a63c06a24ea6d9e84e2e9
Timur Saidov <timur_saidov@smartworld.team>
2021-01-11 17:58:03 +0700

    +- разметка ExerciseDisplay

--- src/Components/ExerciseDisplay/ExerciseDisplay.tsx
+++ src/Components/ExerciseDisplay/ExerciseDisplay.tsx
@@ -1,12 +1,22 @@
 import React from "react";
+import inlinePerson from '../../Assets/inlineSvg/inlinePerson'
 
 import './ExerciseDisplay.css';
+import ExerciseSentence from "../ExerciseSentence";
 
 const ExerciseDisplay: React.FC = () => {
     return(
-        <React.Fragment>
-            ExerciseDisplay
-        </React.Fragment>
+        <div className="exercise-display-container">
+            <div className="exercise-display-person">
+                {inlinePerson}
+            </div>
+
+            <div className="exercise-display-dialog">
+                <ExerciseSentence />
+
+                <div className="exercise-display-dialog-triangle" />
+            </div>
+        </div>
     )
 }
 
