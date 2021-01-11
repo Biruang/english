69b071e532f08dacabcbeb8fecb95bad40cc712a
Timur Saidov <timur_saidov@smartworld.team>
2021-01-11 13:36:24 +0700

    Чистка мусора + шаблон компонента задания

---
+++ src/Containers/Exercise/Exercise.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Exercise: React.FC = () => {
+    return (
+        <React.Fragment>
+            Exercise
+        </React.Fragment>
+    );
+}
+
+export default Exercise;
